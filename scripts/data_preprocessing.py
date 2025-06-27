634cb5c3b0ddf1b5d4ca4dbcb52247e7c01034be
Uddin <mduddin@lm4-870-31673.local>
2025-06-27 11:05:43 +0300

    Added data preprocessing script template

---
+++ scripts/data_preprocessing.py
@@ -0,0 +1,14 @@
+# Data preprocessing script
+import pandas as pd
+import numpy as np
+
+def load_data():
+    '''Function to load and preprocess data'''
+    pass
+
+def clean_data():
+    '''Function to clean the dataset'''
+    pass
+
+if __name__ == '__main__':
+    print('Data preprocessing pipeline')
